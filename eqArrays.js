3fd163121b252e54b89cba73a02d4cf9a39a6f38
bzrkrbddy081 <bzrkrbddy081@gmail.com>
2023-06-06 02:08:35 +0000

    initializing

--- eqArrays.js
+++ eqArrays.js
@@ -1,6 +1,5 @@
 //eqArrays
 
-
 const eqArrays = function(num1, num2) {
   let newNum1 = num1;
   let newNum2 = num2;
@@ -14,17 +13,3 @@ const eqArrays = function(num1, num2) {
 };
 
 module.exports = eqArrays;
-
-
-
-
-// const eqArrays =(array1, array2) => {
-//   if (array1.length === array2.length) {
-//     for (let i = 0; i < array1.length; i++) {
-//     if (array1[i] !== array2[i]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
-// };
